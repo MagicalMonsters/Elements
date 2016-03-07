58a3f6978a853a95e4d9d0ba4f3838369dce6cff
LOG <m.einakchi@gmail.com>
2016-03-06 19:06:13 -0500

    Added back link and join game

--- client/views/game.js
+++ client/views/game.js
@@ -3,3 +3,17 @@ Template.game.helpers({
         return Games.findOne({_id : this.gameId}).name;
     }
 });
+
+Template.game.events({
+    'click a#join-btn': function (e, tpl) {
+        e.preventDefault();
+        Games.update({_id : this.gameId}, {
+            $addToSet: {players: 
+                    {
+                    userName: Meteor.user().username,
+                    userId: Meteor.userId(),
+                }
+            }
+        });
+    }
+})

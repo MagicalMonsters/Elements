1ceccbdeb0dedb6ad7d05ee19a3b56d9a4fdf88e
Behdad <bakhshi.behdad@gmail.com>
2016-03-06 17:11:57 -0700

    leave added

--- client/views/game.js
+++ client/views/game.js
@@ -15,5 +15,17 @@ Template.game.events({
                 }
             }
         });
+    },
+	
+	'click a#leave-btn': function (e, tpl) {
+        e.preventDefault();
+        Games.update({_id : this.gameId}, {
+            $pull: {players: 
+                    {
+                    userName: Meteor.user().username,
+                    userId: Meteor.userId(),
+                }
+            }
+        });
     }
 })

218ce0277eaf0d913c48b2ffa9653dd73a0292fc
Behdad <bakhshi.behdad@gmail.com>
2016-03-06 22:07:35 -0700

    -

---
+++ client/lib/warrior.js
@@ -0,0 +1,9 @@
+Warrior = {};
+
+Warrior.type = function(composition){
+	var max = _.max(composition);
+	var cou = 0;
+	for(int i=0;i<composition.size();i++){
+		
+	}
+}
--- client/views/board.js
+++ client/views/board.js
@@ -12,7 +12,20 @@ Template.board.helpers({
 		
 		for(var i = 0; i < bs; i++) {
 			for(var j = 0; j < bs; j++ ){
-				row.push({ "color" : colors[data.board[i*bs + j]] });
+				var playerID = -1;
+				var warriorType = -1;
+				var warriorLabel = "";
+				for(var k = 0; k < data.players.size() ; k++){
+					var index = _.find(data.players[k].warriors, function(warrior){ return warrior.position == i*bs + j } );
+					if(index != undefined){
+						playerID = k;
+						warriorLabel = data.players[k].warriors[index].label;
+						var composition = data.players[k].warriors[index].composition;
+						Warrior.type(composition);
+						break;
+					}
+				}
+				row.push({ "color" : colors[data.board[i*bs + j]] , "playerClass" : "player"+playerID , "warriorClass" : "warrior"+warriorType , "lable" = warriorLabel });
 			}
 			board.push({ "row" : row });
 			row = [];

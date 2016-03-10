03e62ed0395ff36598f442ed6e3d681b8d3dbc49
LOG <m.einakchi@gmail.com>
2016-03-10 18:16:03 -0500

    An attempt to fix the delete warrior bug

--- server/cli.js
+++ server/cli.js
@@ -46,9 +46,11 @@ Meteor.methods({
 	},
 	
 	'deleteWarrior': function (gameId , userId , warriorLabel) {
+        var game = Games.findOne({_id: gameId});
+        var warriors = _.find(game.players, function (player) {return player.userId == userId;}).warriors;
+        var newWarriors = _.select(warriors, function (warrior){return warrior.lable != warriorLabel;});
         Games.update({_id: gameId, "players.userId": userId}, {
-             $set: {"players.$.warriors": []
-             }
+             $set: {"players.$.warriors": newWarriors}
         });
     },
 });

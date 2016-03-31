8a7e60e65e7c25f052b26b506dfb4a490b27f72f
LOG <m.einakchi@gmail.com>
2016-03-31 16:55:59 -0400

    More refactoring and cleanup

--- client/lib/warrior.js
+++ client/lib/warrior.js
@@ -1,56 +1,59 @@
 Warrior = {};
 
-Warrior.type = function(composition){
-	var max = _.max(composition);
-	var cou = 0;
-	var index = -1;
-	for(var i=0;i<composition.length;i++){
-		if(composition[i] >= max){
-			cou++;
-			index = i;
-		}
-	}
-	
-	if (cou != 1){
-		return -1;
-	}
-	return index;
-}
+Warrior.type = function (composition) {
+    var max = _.max(composition);
+    var cou = 0;
+    var index = -1;
+    for (var i = 0; i < composition.length; i++) {
+        if (composition[i] >= max) {
+            cou++;
+            index = i;
+        }
+    }
 
-Warrior.color = function(warrior){
+    if (cou != 1) {
+        return -1;
+    }
+    return index;
+};
+
+Warrior.color = function (warrior) {
     var colors = ['black', 'brown', 'blue', 'red', 'green'];
     var res = Warrior.type(warrior.composition);
     res++;
     return colors[res];
-}
+};
 
-Warrior.getWarrior = function(gameId, userId, label){
+Warrior.getWarrior = function (gameId, label) {
 
-	var playerWarriors = Warrior.getWarriors(gameId, userId);
+
+    var playerWarriors = Warrior.getWarriors(gameId, Meteor.userId());
 
     return _.find(playerWarriors, function (warrior) {
         return warrior.label == label;
     });
-}
+};
 
 Warrior.canMove = function (warrior) {
     return warrior.moves < 1;
-}
+};
 
-Warrior.getWarriors = function(gameId, userId){
+Warrior.getWarriors = function (gameId, userId) {
     var game = Games.findOne({_id: gameId});
     var playerWarriors = _.find(game.players, function (player) {
         return player.userId == userId;
     }).warriors;
     return playerWarriors;
-}
+};
 
-Warrior.getOwner = function(gameId , position){
-	var game = Games.findOne({_id: gameId});
-	for(var i = 0;i<game.players.length; i++){
-		var Id = game.players[i].userId;
-		if(!(_.isUndefined(_.find(game.players[i].warriors, function (warrior) {return warrior.position == position; })))){
-			return Id;
-		}
-	}
-}
+Warrior.getOwner = function (gameId, position) {
+    var game = Games.findOne({_id: gameId});
+    for (var i = 0; i < game.players.length; i++) {
+        var Id = game.players[i].userId;
+        if (!(_.isUndefined(_.find(game.players[i].warriors, function (warrior) {
+                return warrior.position == position;
+            })))) {
+            return Id;
+        }
+    }
+};
--- client/views/cli.js
+++ client/views/cli.js
@@ -35,7 +35,7 @@ function submit(gameId, command, e) {
 }
 
 function parse(gameId, command) {
-    if(!Logic.isMyTurn(gameId, Meteor.userId())){
+    if (!Logic.isMyTurn(gameId, Meteor.userId())) {
         return "This is not your turn";
     }
     if (_.isEmpty(command)) {
@@ -46,20 +46,20 @@ function parse(gameId, command) {
         return "";
     }
     var action = tokens[0].toLowerCase();
-	
+
     if (action == "create") {
         return create(gameId, tokens[1], tokens[2]);
     } else if (action == "move") {
         return move(gameId, tokens[1], tokens[2]);
     } else if (action == "add") {
-        return add(gameId, tokens[1], tokens[2]);    
+        return add(gameId, tokens[1], tokens[2]);
     } else if (action == "split") {
-		return spliting(gameId, tokens[1], tokens[2], tokens[3]);
+        return spliting(gameId, tokens[1], tokens[2], tokens[3]);
     } else if (action == "merge") {
-		return merging(gameId, tokens[1], tokens[2]);
-    }else if (action == "end") {
+        return merging(gameId, tokens[1], tokens[2]);
+    } else if (action == "end") {
         Session.set("inProgress", Session.get("inProgress") + 1);
-        Meteor.call("endTurn", gameId , Meteor.userId(), function (error, result) {
+        Meteor.call("endTurn", gameId, Meteor.userId(), function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
     } else {
@@ -78,30 +78,30 @@ function create(gameId, elements, coordination) {
         return memo + num;
     }, 0);
 
-	var warriors = Warrior.getWarriors(gameId, Meteor.userId());
-	
-	if(warriors.length > 0){
-		return "You can't create more than one warrior";
-	}
-	
+    var warriors = Warrior.getWarriors(gameId, Meteor.userId());
+
+    if (warriors.length > 0) {
+        return "You can't create more than one warrior";
+    }
+
     if (sum != 20) {
         return "The sum should be 20";
     }
-    
-    
-	if(!Logic.isFirstRound(gameId)){
-		return "You can create only at first turn";
-	}
 
-    // TODO: should remove this 
+
+    if (!Logic.isFirstRound(gameId)) {
+        return "You can create only at first turn";
+    }
+
+    // TODO: should remove this
     var game = Games.findOne({_id: gameId});
 
     coordination = coordination.split(",");
     var boardCellIndex = parseInt(coordination[1]) + parseInt(coordination[0]) * game.boardSize;
-	
-	if( Board.cellType(game, boardCellIndex).type != "empty" ) {
-		return "Not a valid position";
-	}
+
+    if (Board.cellType(game, boardCellIndex).type != "empty") {
+        return "Not a valid position";
+    }
     Session.set("inProgress", Session.get("inProgress") + 1);
     Meteor.call("createWarrior", gameId, Meteor.userId(), boardCellIndex, elems, function (error, result) {
         Session.set("inProgress", Session.get("inProgress") - 1);
@@ -111,246 +111,260 @@ function create(gameId, elements, coordination) {
 function move(gameId, label, direction) {
 
     var game = Games.findOne({_id: gameId});
-	
-	if(game.turn < game.players.length){
-		return "You can't move at first turn";
-	}
-	
-	var warrior = Warrior.getWarrior(gameId, Meteor.userId(), label);
+
+    if (Logic.isFirstRound(gameId)) {
+        return "You can't move at first turn";
+    }
+
+    var warrior = Warrior.getWarrior(gameId, label);
 
     if (warrior == undefined) {
         return "No warrior with this label.";
     }
-	
-	if(!canMove(warrior)){
-		return "You can't move any more in this turn";
-	}
-    
-	var cellToMove = Board.directionOfCell(game, warrior.position, direction);
-	
-	if( cellToMove < -50 ) {
-		return "Invalid direction";
-	}
-	
-	var cellType = Board.cellType(game, cellToMove);
-	
-	if(cellType.type == "wall"){
-		return "Cannot move to that direction. (wall)";
-	}
-	
-	else if(cellType.type == "empty"){
+
+    if (!canMove(warrior)) {
+        return "You can't move any more in this turn";
+    }
+
+    var cellToMove = Board.directionOfCell(game, warrior.position, direction);
+
+    if (cellToMove < -50) {
+        return "Invalid direction";
+    }
+
+    var cellType = Board.cellType(game, cellToMove);
+
+    if (cellType.type == "wall") {
+        return "Cannot move to that direction. (wall)";
+    }
+
+    else if (cellType.type == "empty") {
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorActs", gameId, Meteor.userId() , warrior.label, function (error, result) {
+        Meteor.call("warriorActs", gameId, Meteor.userId(), warrior.label, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetPosition", gameId, Meteor.userId(), warrior.label, cellToMove, function (error, result) {
+        Meteor.call("warriorSetPosition", gameId, Meteor.userId(), warrior.label, cellToMove, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
-	}
-	else{
-	
-		otherWarrior = cellType.warrior;
-		if(!(_.isUndefined(_.find(Warrior.getWarriors(gameId,Meteor.userId()), function (warrior) { return warrior.position == otherWarrior.position;})))){
-			return "Cannot move to that direction. (your warrior)";
-		}
-		else{
+    }
+    else {
+
+        otherWarrior = cellType.warrior;
+        if (!(_.isUndefined(_.find(Warrior.getWarriors(gameId, Meteor.userId()), function (warrior) {
+                return warrior.position == otherWarrior.position;
+            })))) {
+            return "Cannot move to that direction. (your warrior)";
+        }
+        else {
             Session.set("inProgress", Session.get("inProgress") + 1);
-			Meteor.call("warriorActs", gameId, Meteor.userId() , warrior.label, function (error, result) {
+            Meteor.call("warriorActs", gameId, Meteor.userId(), warrior.label, function (error, result) {
                 Session.set("inProgress", Session.get("inProgress") - 1);
             });
-			return attack(gameId, warrior, otherWarrior);
-		}
-	}
+            return attack(gameId, warrior, otherWarrior);
+        }
+    }
 }
 
-function attack(gameId, warrior1, warrior2){
+function attack(gameId, warrior1, warrior2) {
 
-	var result = Logic.calculateAttack(warrior1.composition, warrior2.composition);
-	
-	var otherId = Warrior.getOwner(gameId,warrior2.position);
-	
-	if( _.reduce(result.composition2, function(memo, num){ return memo + num; }, 0) == 0 ){ // Defender died
-		
-		var newPos = warrior2.position;
-		var newbackpack = warrior2.backpack;
-		newbackpack = _.map(newbackpack, function(num, index){ return num + warrior1.backpack[index]; });
+    var result = Logic.calculateAttack(warrior1.composition, warrior2.composition);
+
+    var otherId = Warrior.getOwner(gameId, warrior2.position);
+
+    if (_.reduce(result.composition2, function (memo, num) {
+            return memo + num;
+        }, 0) == 0) { // Defender died
+
+        var newPos = warrior2.position;
+        var newbackpack = warrior2.backpack;
+        newbackpack = _.map(newbackpack, function (num, index) {
+            return num + warrior1.backpack[index];
+        });
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("deleteWarrior", gameId, otherId, warrior2.label, function (error, result) {
+        Meteor.call("deleteWarrior", gameId, otherId, warrior2.label, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetPosition", gameId, warrior1.label, newPos, function (error, result) {
+        Meteor.call("warriorSetPosition", gameId, warrior1.label, newPos, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetComposition", gameId, Meteor.userId() , warrior1.label, result.composition1,
+        Meteor.call("warriorSetComposition", gameId, Meteor.userId(), warrior1.label, result.composition1,
             false, newbackpack, function (error, result) {
                 Session.set("inProgress", Session.get("inProgress") - 1);
             });
-	}
-	else if( _.reduce(result.composition1, function(memo, num){ return memo + num; }, 0) == 0 ){ //Attacker died
+    }
+    else if (_.reduce(result.composition1, function (memo, num) {
+            return memo + num;
+        }, 0) == 0) { //Attacker died
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("deleteWarrior", gameId, Meteor.userId(), warrior1.label, function (error, result) {
+        Meteor.call("deleteWarrior", gameId, Meteor.userId(), warrior1.label, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetComposition", gameId, otherId,
+        Meteor.call("warriorSetComposition", gameId, otherId,
             warrior2.label, result.composition2, function (error, result) {
-            Session.set("inProgress", Session.get("inProgress") - 1);
-        });
-	}
-	else {
+                Session.set("inProgress", Session.get("inProgress") - 1);
+            });
+    }
+    else {
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetComposition", gameId, Meteor.userId() ,warrior1.label,
+        Meteor.call("warriorSetComposition", gameId, Meteor.userId(), warrior1.label,
             result.composition1, function (error, result) {
-            Session.set("inProgress", Session.get("inProgress") - 1);
-        });
+                Session.set("inProgress", Session.get("inProgress") - 1);
+            });
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetComposition", gameId, otherId ,warrior2.label,
+        Meteor.call("warriorSetComposition", gameId, otherId, warrior2.label,
             result.composition2, function (error, result) {
-            Session.set("inProgress", Session.get("inProgress") - 1);
-        });
-	}
-	
-	return result.message;
+                Session.set("inProgress", Session.get("inProgress") - 1);
+            });
+    }
+
+    return result.message;
 }
 
-function canMove(warrior){
+function canMove(warrior) {
 
-	if(warrior.moves < 1){
-		return true;
-	}
-	return false;
+    if (warrior.moves < 1) {
+        return true;
+    }
+    return false;
 }
 
-function add(gameId, label, elements){
-	var game = Games.findOne({_id: gameId});
-	var elems = elements.split(",");
-	elems = _(elems).map(function (elem) {
+function add(gameId, label, elements) {
+    var game = Games.findOne({_id: gameId});
+    var elems = elements.split(",");
+    elems = _(elems).map(function (elem) {
         return parseInt(elem);
     });
-	var warrior = Warrior.getWarrior(gameId, Meteor.userId(), label);
-	if (warrior == undefined) {
+    var warrior = Warrior.getWarrior(gameId, label);
+    if (warrior == undefined) {
         return "No warrior with this label.";
     }
-	var newBackpack = warrior.backpack;
-	if(warrior.turnsToReincarnation > 0){
-		return "You can't reincarnat yet";
-	}
-	for(var i = 0;i<elems.length;i++){
-		if(elems[i] > warrior.backpack[i]){
-			return "This is more than what you have in backpack";
-		}
-		newBackpack[i] -= elems[i];
-		elems[i] += warrior.composition[i];
-	}
+    var newBackpack = warrior.backpack;
+    if (warrior.turnsToReincarnation > 0) {
+        return "You can't reincarnat yet";
+    }
+    for (var i = 0; i < elems.length; i++) {
+        if (elems[i] > warrior.backpack[i]) {
+            return "This is more than what you have in backpack";
+        }
+        newBackpack[i] -= elems[i];
+        elems[i] += warrior.composition[i];
+    }
 
     Session.set("inProgress", Session.get("inProgress") + 1);
-	Meteor.call("warriorSetComposition", gameId, Meteor.userId() ,label, elems,true ,
+    Meteor.call("warriorSetComposition", gameId, Meteor.userId(), label, elems, true,
         newBackpack, function (error, result) {
-        Session.set("inProgress", Session.get("inProgress") - 1);
-    });
+            Session.set("inProgress", Session.get("inProgress") - 1);
+        });
 }
 
-function spliting(gameId, label, elements, direction){
-	var game = Games.findOne({_id: gameId});
-	
-	var elems = elements.split(",");
-	elems = _(elems).map(function (elem) {
+function spliting(gameId, label, elements, direction) {
+    var game = Games.findOne({_id: gameId});
+
+    var elems = elements.split(",");
+    elems = _(elems).map(function (elem) {
         return parseInt(elem);
     });
-	
-	var warrior = Warrior.getWarrior(gameId, Meteor.userId(), label);
+
+    var warrior = Warrior.getWarrior(gameId, label);
 
     if (warrior == undefined) {
         return "No warrior with this label";
     }
-	
-	if( !warrior.canSplit ) {
-		return "you can't split yet";
-	}
-	
-	var newComp = warrior.composition;
-	
-	for(var i = 0;i<elems.length;i++){
-		if(elems[i] > warrior.composition[i]){
-			return "This is more than what you have in composition";
-		}
-		newComp[i] -= elems[i];
-	}
-    
-	var cellToMove = Board.directionOfCell(game, warrior.position, direction);
-	
-	if( cellToMove < -50 ) {
-		return "Invalid direction";
-	}
-	
-	var cellType = Board.cellType(game, cellToMove);
-	
-	if(cellType.type != "empty"){
-		return "You can't move to that direction";
-	}
-	
-	if(_.reduce(elems, function(memo, num){ return memo + num; }, 0) != 0 ){
+
+    if (!warrior.canSplit) {
+        return "you can't split yet";
+    }
+
+    var newComp = warrior.composition;
+
+    for (var i = 0; i < elems.length; i++) {
+        if (elems[i] > warrior.composition[i]) {
+            return "This is more than what you have in composition";
+        }
+        newComp[i] -= elems[i];
+    }
+
+    var cellToMove = Board.directionOfCell(game, warrior.position, direction);
+
+    if (cellToMove < -50) {
+        return "Invalid direction";
+    }
+
+    var cellType = Board.cellType(game, cellToMove);
+
+    if (cellType.type != "empty") {
+        return "You can't move to that direction";
+    }
+
+    if (_.reduce(elems, function (memo, num) {
+            return memo + num;
+        }, 0) != 0) {
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("createWarrior", gameId, Meteor.userId(), cellToMove, elems, function (error, result) {
+        Meteor.call("createWarrior", gameId, Meteor.userId(), cellToMove, elems, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
-	}
-	
-	if( _.reduce(newComp, function(memo, num){ return memo + num; }, 0) == 0 ){
+    }
+
+    if (_.reduce(newComp, function (memo, num) {
+            return memo + num;
+        }, 0) == 0) {
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("deleteWarrior", gameId, Meteor.userId(), label, function (error, result) {
+        Meteor.call("deleteWarrior", gameId, Meteor.userId(), label, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
-	}
-	else{
+    }
+    else {
         Session.set("inProgress", Session.get("inProgress") + 1);
-		Meteor.call("warriorSetComposition", gameId, Meteor.userId() , label, newComp, function (error, result) {
+        Meteor.call("warriorSetComposition", gameId, Meteor.userId(), label, newComp, function (error, result) {
             Session.set("inProgress", Session.get("inProgress") - 1);
         });
-	}
+    }
 }
 
-function merging(gameId, label, direction){
-	var game = Games.findOne({_id: gameId});
-	
-	var warrior = Warrior.getWarrior(gameId, Meteor.userId(), label);
+function merging(gameId, label, direction) {
+    var game = Games.findOne({_id: gameId});
+
+    var warrior = Warrior.getWarrior(gameId, label);
 
     if (warrior == undefined) {
         return "No warrior with this label";
     }
-	
-	if( !warrior.canSplit ) {
-		return "you can't merge yet";
-	}
-	
-	var comp = warrior.composition;
-    
-	var cellToMove = Board.directionOfCell(game, warrior.position, direction);
-	
-	if( cellToMove < -50 ) {
-		return "Invalid direction";
-	}
-	
-	var cellType = Board.cellType(game, cellToMove);
-	
-	if(cellType.type != "warrior"){
-		return "There is no warrior at that direction";
-	}
-	else if(Warrior.getOwner(gameId, cellType.warrior.position) != Meteor.userId() ){
-		return "You can only merge with your own warriors";
-	}
-	
-	newComp = _.map(cellType.warrior.composition , function (num, index){ return num + comp[index]} );
-	
-	Session.set("inProgress", Session.get("inProgress") + 1);
-	Meteor.call("warriorSetComposition", gameId, Meteor.userId() , cellType.warrior.label, newComp, function (error, result) {
+
+    if (!warrior.canSplit) {
+        return "you can't merge yet";
+    }
+
+    var comp = warrior.composition;
+
+    var cellToMove = Board.directionOfCell(game, warrior.position, direction);
+
+    if (cellToMove < -50) {
+        return "Invalid direction";
+    }
+
+    var cellType = Board.cellType(game, cellToMove);
+
+    if (cellType.type != "warrior") {
+        return "There is no warrior at that direction";
+    }
+    else if (Warrior.getOwner(gameId, cellType.warrior.position) != Meteor.userId()) {
+        return "You can only merge with your own warriors";
+    }
+
+    newComp = _.map(cellType.warrior.composition, function (num, index) {
+        return num + comp[index]
+    });
+
+    Session.set("inProgress", Session.get("inProgress") + 1);
+    Meteor.call("warriorSetComposition", gameId, Meteor.userId(), cellType.warrior.label, newComp, function (error, result) {
         Session.set("inProgress", Session.get("inProgress") - 1);
     });
-	Session.set("inProgress", Session.get("inProgress") + 1);
-	Meteor.call("deleteWarrior", gameId, Meteor.userId(), warrior.label, function (error, result) {
+    Session.set("inProgress", Session.get("inProgress") + 1);
+    Meteor.call("deleteWarrior", gameId, Meteor.userId(), warrior.label, function (error, result) {
         Session.set("inProgress", Session.get("inProgress") - 1);
     });
 }

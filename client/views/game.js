99f19cc123a56ec6988b86cc38c518e3cdeb93ee
LOG <m.einakchi@gmail.com>
2016-04-02 01:10:01 -0400

    Fixed the board creating bug for even numbers

--- client/views/game.js
+++ client/views/game.js
@@ -1,45 +1,49 @@
 Template.game.helpers({
     name: function () {
-        return Games.findOne({_id : this.gameId}).name;
+        return Games.findOne({_id: this.gameId}).name;
     },
-	canJoin: function (){
-		var game = Games.findOne({_id: this.gameId});
-        return (Meteor.userId() != game.owner.userId) && (game.turn == -1) && _.find(game.players, function (player){ return player.userId == Meteor.userId(); }) == undefined;
-	},
+    canJoin: function () {
+        var game = Games.findOne({_id: this.gameId});
+        return (Meteor.userId() != game.owner.userId) && (game.turn == -1) && _.find(game.players, function (player) {
+                return player.userId == Meteor.userId();
+            }) == undefined;
+    },
     canStart: function () {
-		var game = Games.findOne({_id: this.gameId});
+        var game = Games.findOne({_id: this.gameId});
         return (Meteor.userId() == game.owner.userId) && (game.turn == -1);
     },
-	isStarted: function () {
+    isStarted: function () {
         return Games.findOne({_id: this.gameId}).turn >= 0;
-	},
+    },
 });
 
-function createBoard(bs){
+function createBoard(bs) {
 
-	var board = [];
-	var active = Math.floor(bs/2);
-	for(var i = 0;i<bs ;i++){
-		for(var j = 0;j<bs ; j++){
-			if(j < Math.abs(active) || j >= bs-Math.abs(active)){
-				board.push(0);
-			}
-			else{
-				board.push(Math.floor(Math.random()*4.0)+1);
-			}
-		}
-		active--;
-	}
-	
-	return board;
+    var board = [];
+    var active = Math.floor((bs - 1) / 2);
+    for (var i = 0; i < bs; i++) {
+        for (var j = 0; j < bs; j++) {
+            if (j < Math.abs(active) || j >= bs - Math.abs(active)) {
+                board.push(0);
+            }
+            else {
+                board.push(Math.floor(Math.random() * 4.0) + 1);
+            }
+        }
+        if (i != bs/2 -1) {
+            active--;
+        }
+    }
+
+    return board;
 }
 
 Template.game.events({
     'click a#join-btn': function (e, tpl) {
         e.preventDefault();
-        Games.update({_id : this.gameId}, {
-            $addToSet: {players: 
-                    {
+        Games.update({_id: this.gameId}, {
+            $addToSet: {
+                players: {
                     userName: Meteor.user().username,
                     userId: Meteor.userId(),
                     warriors: [],
@@ -47,26 +51,26 @@ Template.game.events({
             }
         });
     },
-	
-	'click a#leave-btn': function (e, tpl) {
+
+    'click a#leave-btn': function (e, tpl) {
         e.preventDefault();
-        Games.update({_id : this.gameId}, {
-            $pull: {players: 
-                    {
+        Games.update({_id: this.gameId}, {
+            $pull: {
+                players: {
                     userId: Meteor.userId(),
                 }
             }
         });
     },
-	
-	'click a#start-btn': function (e, tpl) {
-		e.preventDefault();
-		Games.update({_id : this.gameId}, {
-			$set: {
-				turn: 0,
-				boardSize: 8,
-				board: createBoard(8)
-			}
-		});
-	}
+
+    'click a#start-btn': function (e, tpl) {
+        e.preventDefault();
+        Games.update({_id: this.gameId}, {
+            $set: {
+                turn: 0,
+                boardSize: 8,
+                board: createBoard(8)
+            }
+        });
+    }
 })
